f00bb06d4d6119507c0f654d4d7fa7ddb19107bc
AdamTasma <adamtasma@gmail.com>
2018-02-21 15:24:11 -0500

    first commit

---
+++ FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class FizzBuzzTest
+    {
+        [Test]
+        public void EnterOne()
+        {
+            string expected = "1";
+            string actual = Program.FizzBuzzMethod("1");
+            Assert.AreEqual(expected, actual, "1 should return 1");
+        }
+        [Test]
+        public void EnterThree() {
+            string expected = "Fizz";
+            string actual = Program.FizzBuzzMethod("3");
+            Assert.AreEqual(expected, actual, "3 should return Fizz");
+        }
+        [Test]
+        public void EnterTen() {
+            string expected = "Buzz";
+            string actual = Program.FizzBuzzMethod("10");
+            Assert.AreEqual(expected, actual, "10 should return Buzz");
+        }
+        [Test]
+        public void EnterThirty() { 
+            string expected = "FizzBuzz";
+            string actual = Program.FizzBuzzMethod("30");
+            Assert.AreEqual(expected, actual, "30 should return FizzBuzz");
+        }
+
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //counts 1-100
+            //if multiple of 3 returns fizz
+            //if multiple of 5 returns buzz
+            //if multiple of both, returns fizzbuzz
+            Console.WriteLine("1" == "Something");
+            string str = "15";
+            Console.WriteLine(FizzBuzzMethod(str)); 
+        }
+        public static string FizzBuzzMethod(string str)
+        {
+            int i = int.Parse(str);
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    return "FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    return "Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    return "Buzz";
+                }
+            }
+            return str;          
+        }
+    }
+}
